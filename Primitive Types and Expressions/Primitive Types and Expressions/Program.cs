0d96efb9a9fe77d2bc7b4f5ac7d1dccec9b46f5e
Błażej Domagała <petitoff@outlook.com>
2022-03-01 16:47:30 +0100

    Variables and Constants

--- Primitive Types and Expressions/Primitive Types and Expressions/Program.cs
+++ Primitive Types and Expressions/Primitive Types and Expressions/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Primitive_Types_and_Expressions
 {
@@ -10,6 +6,33 @@ namespace Primitive_Types_and_Expressions
     {
         static void Main(string[] args)
         {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95F;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = true;
+
+            var numberV = 2;
+            var countV = 10;
+            var totalPriceV = 20.95F;
+            var characterV = 'A';
+            var firstNameV = "Mosh";
+            var isWorkingV = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.Write("Here: ");
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+            // Pi = 1; // error
         }
     }
 }
