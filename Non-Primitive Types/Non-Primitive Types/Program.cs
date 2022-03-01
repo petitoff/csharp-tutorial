6a5fb51f7dd465b54f5937985792cd624c855cf8
Błażej Domagała <petitoff@outlook.com>
2022-03-01 18:05:14 +0100

    Non-Primitive Types

---
+++ Non-Primitive Types/Non-Primitive Types/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_Primitive_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
