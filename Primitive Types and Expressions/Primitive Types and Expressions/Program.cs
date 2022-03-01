142ec5b1aaba95772bcbb2f4a0083e63bf2cd07c
Błażej Domagała <petitoff@outlook.com>
2022-03-01 16:32:43 +0100

    Add new section

---
+++ Primitive Types and Expressions/Primitive Types and Expressions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primitive_Types_and_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
