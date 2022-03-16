55aecac5c3a31bf84f8e74e033d3a37d0a653ce7
Błażej Domagała <petitoff@outlook.com>
2022-03-16 21:49:13 +0100

    35. Demo: Enums

---
+++ Non-Primitive Types/enums/enums/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisterAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId); // output: Express
+
+            Console.WriteLine(method.ToString());
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
