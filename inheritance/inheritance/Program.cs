0e5262f7c2a08608f2cefde673080b8af9e130b1
Błażej Domagała <petitoff@outlook.com>
2022-05-13 14:32:20 +0200

    Creating class inheritance

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Program
+    {
+        class Animal
+        {
+            public string name;
+            public int age;
+            public float happiness;
+
+            public void PrintBase()
+            {
+                Console.WriteLine($"Name {name}");
+                Console.WriteLine($"Age {age}");
+                Console.WriteLine($"Happiness {happiness}");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public int spotCount;
+
+            public void Bark()
+            {
+                Console.WriteLine("WUF!");
+                base.happiness += 0.1f;
+            }
+        }
+
+        class Cat : Animal
+        {
+            public float cuteness;
+
+            public void Meow()
+            {
+                Console.WriteLine("MEOW!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog spotty = new Dog();
+            spotty.name = "Spotty";
+            spotty.age = 4;
+            spotty.happiness = 0.8f;
+            spotty.spotCount = 25;
+            spotty.PrintBase();
+            spotty.Bark();
+            Console.WriteLine($"New Hapiness {spotty.happiness}");
+
+            Cat heisenberg = new Cat();
+            heisenberg.name = "Heisenberg";
+            heisenberg.age = 13;
+            heisenberg.happiness = 0.3f;
+            heisenberg.cuteness = 0;
+            heisenberg.cuteness = 0.4f;
+            heisenberg.PrintBase();
+            heisenberg.Meow();
+        }
+    }
+}
