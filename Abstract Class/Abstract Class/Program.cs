764e6082586738c3d6330e011bc7edf6c7441bd7
Błażej Domagała <petitoff@outlook.com>
2022-05-20 13:30:37 +0200

    Abstract Class

---
+++ Abstract Class/Abstract Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = {new Circle(5), new Rectangle(4, 5)};
+
+            foreach (Shape s in shapes)
+            {
+                s.GetInfo();
+                Console.WriteLine("{0} Area : {1:f2}", s.Name, s.Area());
+
+                Circle testCircle = s as Circle;
+                if (testCircle == null)
+                {
+                    Console.WriteLine("This isn't a Circle");
+                }
+
+                if (s is Circle)
+                {
+                    Console.WriteLine("This isn't a Rectangle");
+                }
+
+                Console.WriteLine();
+
+                object circ1 = new Circle(4);
+                Circle circ2 = (Circle) circ1;
+                Console.WriteLine("The {0} Area is {1:f2}", circ2.Name, circ2.Area());
+            }
+        }
+    }
+}
---
+++ Abstract Class/Abstract Class/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Rectangle : Shape
+    {
+        public double Length { set; get; }
+        public double Width { set; get; }
+
+        public Rectangle(double length, double width)
+        {
+            Name = "Rectangle";
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"It has a Length of {Length} and Width of {Width}");
+        }
+    }
+}
