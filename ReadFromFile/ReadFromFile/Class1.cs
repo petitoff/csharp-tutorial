de4ef60216eb739ec80dfcc1cad5afa74e3b1f27
Błażej Domagała <petitoff@outlook.com>
2022-06-04 10:23:20 +0000

    wczytywanie tekstu z pliku do ListBox

---
+++ ReadFromFile/ReadFromFile/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadFromFile
+{
+    internal class Class1
+    {
+        private string _filePath;
+
+        public Class1(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        public void ReadFromFile(ListBox listBox)
+        {
+            StreamReader streamReader = new StreamReader(_filePath);
+            List<string> lineOfText = new List<string>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                lineOfText.Add(line);
+            }
+
+            foreach (string s in lineOfText)
+            {
+                listBox.Items.Add(s);
+            }
+        }
+    }
+}
---
+++ ReadFromFile/ReadFromFile/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadFromFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 class1 = new Class1(textBox1.Text);
+            class1.ReadFromFile(listBox1);
+        }
+    }
+}
