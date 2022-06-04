d273fabeb6a25487f73873805d063399d59691b0
Błażej Domagała <petitoff@outlook.com>
2022-06-04 11:23:02 +0000

    Zapis listbox do pliku

---
+++ WriteToFile/WriteToFile/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WriteToFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Class1 class1 = new Class1();
+            class1.WriteToFile(listBox1, textBox2);
+        }
+    }
+}
