6afd9067eb0279758885b76a8cb50e6ccb62ae4d
Błażej Domagała <petitoff@outlook.com>
2022-07-03 13:15:13 +0200

    funkcje operatorowe

---
+++ operatoryDemo1/operatoryDemo1/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatoryDemo1
+{
+    class Shape
+    {
+        public int a;
+        public bool b;
+
+        public Shape()
+        {
+        }
+
+        public Shape(int a)
+        {
+            this.a = a;
+        }
+
+        public static Shape operator +(Shape shape1, Shape shape2)
+        {
+            Shape sum = new Shape();
+            sum.a = shape1.a + shape2.a;
+
+            return sum;
+        }
+
+        public static Shape operator ==(Shape shape1, Shape shape2)
+        {
+            Shape shape = new Shape();
+            if (shape1.a == shape2.a) shape.b = true;
+            else shape.b = false;
+
+            return shape;
+        }
+
+        public static Shape operator !=(Shape shape1, Shape shape2)
+        {
+            return null;
+        }
+            
+    }
+}
