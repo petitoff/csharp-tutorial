91eb54dc7320d9280ae9d6b847ad8a295d39942f
Błażej Domagała <petitoff@outlook.com>
2022-07-06 20:11:33 +0200

    get and set with if method for validation

---
+++ GetandSet/GetandSet/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetandSet
+{
+    class Car
+    {
+        private int _speed;
+
+        public int Speed
+        {
+            get => _speed;
+            set => _speed = value > 500 ? 500 : value;
+        }
+    }
+}
