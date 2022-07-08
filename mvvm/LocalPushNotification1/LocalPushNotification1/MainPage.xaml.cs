0428a95e450e565e22e003b9ebc58c58cdac9b12
Błażej Domagała <petitoff@outlook.com>
2022-07-08 15:19:09 +0200

    PoC push notification

---
+++ mvvm/LocalPushNotification1/LocalPushNotification1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Plugin.LocalNotification;
+
+namespace LocalPushNotification1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		var notification = new NotificationRequest
+		{
+			BadgeNumber = 1,
+			Description = "Text description",
+			Title = "Notification!",
+			ReturningData = "Dummy Data",
+			Schedule =
+			{
+				NotifyTime = DateTime.Now.AddSeconds(5)
+			}
+        };
+		NotificationCenter.Current.Show(notification);
+	}
+}
+
