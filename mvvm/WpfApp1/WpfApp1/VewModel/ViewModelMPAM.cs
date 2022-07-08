868a7a2ad19482593dae8bf0e80f37f9fabb8d63
Błażej Domagała <petitoff@outlook.com>
2022-07-08 17:43:47 +0000

    mvvm

---
+++ mvvm/WpfApp1/WpfApp1/VewModel/ViewModelMPAM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.VewModel
+{
+    public class ViewModelMPAM : INotifyPropertyChanged
+    {
+        private ModelMPAP modelMpap = new ModelMPAP();
+
+        public double Value
+        {
+            get { return modelMpap.Value; }
+            set
+            {
+                modelMpap.Value = value;
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
