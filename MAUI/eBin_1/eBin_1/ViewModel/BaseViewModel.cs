1b3ff440a4a5b255fd67ea5f3a0b7bf318fa9d68
Błażej Domagała <petitoff@outlook.com>
2022-07-12 13:33:17 +0200

    Create BaseViewModel.cs

---
+++ MAUI/eBin_1/eBin_1/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBin_1.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
