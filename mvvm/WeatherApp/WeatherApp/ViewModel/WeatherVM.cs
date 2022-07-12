8f84974d4422d0e600094251a6e60e931ab67eaa
Błażej Domagała <petitoff@outlook.com>
2022-07-12 13:35:51 +0200

    Comment
    Remove comment code from file

--- mvvm/WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
+++ mvvm/WeatherApp/WeatherApp/ViewModel/WeatherVM.cs
@@ -57,27 +57,7 @@ namespace WeatherApp.ViewModel
         public SearchCommand SearchCommand { get; set; }
 
         public WeatherVM()
-        {
-            //if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
-            //{
-            //    SelectedCity = new City
-            //    {
-            //        LocalizedName = "New York"
-            //    };
-
-            //    CurrentConditions = new CurrentConditions
-            //    {
-            //        WeatherText = "Partly cloudy",
-            //        Temperature = new Temperature
-            //        {
-            //            Metric = new Units
-            //            {
-            //                Value = "21"
-            //            }
-            //        }
-            //    };
-            //}
-
+        { 
             SearchCommand = new SearchCommand(this);
             Cities = new ObservableCollection<City>();
         }
