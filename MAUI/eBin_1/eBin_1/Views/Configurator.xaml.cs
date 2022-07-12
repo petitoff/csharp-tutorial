800598a2e7e6fbde081d998faf7214d8568d3dda
Błażej Domagała <petitoff@outlook.com>
2022-07-12 10:38:23 +0200

    Update Configurator.xaml.cs

--- MAUI/eBin_1/eBin_1/Views/Configurator.xaml.cs
+++ MAUI/eBin_1/eBin_1/Views/Configurator.xaml.cs
@@ -1,3 +1,5 @@
+using eBin_1.ViewModel;
+
 namespace eBin_1.Views;
 
 public partial class Configurator : ContentPage
@@ -5,5 +7,6 @@ public partial class Configurator : ContentPage
 	public Configurator()
 	{
 		InitializeComponent();
+		BindingContext = new ConfiguratorViewModel();
 	}
 }
