70f222997e67ec1c2d00e93da93dade17e502732
Błażej Domagała <petitoff@outlook.com>
2022-07-12 10:38:38 +0200

    INotifyPropertyChanged for entry in xaml

---
+++ MAUI/eBin_1/eBin_1/ViewModel/ConfiguratorViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace eBin_1.ViewModel
+{
+    public class ConfiguratorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string street;
+        public string Street
+        {
+            get => street;
+            set
+            {
+                if (street != value)
+                {
+                    street = value;
+                    OnPropertyChanged();
+                }
+            }
+
+        }
+    }
+}
