b57517733da4ef8bdbd379515ea9f9712e42f1dd
Błażej Domagała <petitoff@outlook.com>
2022-07-12 14:54:57 +0200

    Update BaseViewModel.cs

--- MAUI/eBin_1/eBin_1/ViewModel/BaseViewModel.cs
+++ MAUI/eBin_1/eBin_1/ViewModel/BaseViewModel.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace eBin_1.ViewModel;
 
-namespace eBin_1.ViewModel
+public class BaseViewModel : INotifyPropertyChanged
 {
-    public class BaseViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangedEventHandler PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
