042a605d96fa2529b8859a98abca0d6da21e652d
Błażej Domagała <petitoff@outlook.com>
2022-07-14 10:45:56 +0200

    Update MainPage.xaml.cs

--- MAUI/Phoneword/Phoneword/MainPage.xaml.cs
+++ MAUI/Phoneword/Phoneword/MainPage.xaml.cs
@@ -7,9 +7,35 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	{
+    string translatedNumber;
 
+    private void OnTranslate(object sender, EventArgs e)
+	{
+		string enteredNumber = PhoneNumberText.Text;
+		translatedNumber = Phoneword.PhonewordTranslator.ToNumber(enteredNumber);
+
+		if (!string.IsNullOrEmpty(enteredNumber))
+		{
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
 		else
+		{
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
 	}
+
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+        "Dial a Number",
+        "Would you like to call " + translatedNumber + "?",
+        "Yes",
+        "No"))
+        {
+            // TODO: dial the phone
+        }
+    }
 }
 
