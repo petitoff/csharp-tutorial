f5c7c1d2d2872dc9396246ea92bb479fd8db9ba7
Błażej Domagała <petitoff@outlook.com>
2022-07-21 14:50:11 +0200

    Added testing program

---
+++ DateTime/DateTimeParseFormat/DateTimeParseFormat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeParseFormat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mainDate = "10.7.2022";
+            DateTime dateString = DateTime.Parse(mainDate);
+
+            Console.WriteLine(dateString);
+        }
+    }
+}
