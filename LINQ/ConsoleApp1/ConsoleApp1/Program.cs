a1e83e41fdbbbabcccb3d75877c3881e57c72c4a
Błażej Domagała <petitoff@outlook.com>
2022-07-30 15:52:29 +0200

    added courses about grid in wpf and linq

---
+++ LINQ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfInts = new List<int>
+            {
+                3,7,8,11,2,1
+            };
+
+            var listOfStrings = new List<string>
+            {
+                "Ala",
+                "Ma",
+                "Dużego",
+                "Kota",
+                "I",
+                "",
+                "Samochód"
+            };
+
+            var listOfUsers = new List<User>
+            {
+                new User
+                {
+                    FirstName = "Błażej",
+                    LastName = "Domagała",
+                    Email = "petitoff@outlook.com"
+                },
+                new User
+                {
+                    FirstName = "Maciek",
+                    LastName = "Fajny",
+                    Email = "petitoff@outlook.com"
+                },
+                new User
+                {
+                    FirstName = "Piotr",
+                    Email = "innymail@outlook.com"
+                },
+                null
+            };
+
+            var list1 = listOfInts.Select(x => x + 5).ToList();
+
+            var listOfLastNameUsers = listOfUsers.Where(u => u != null && !string.IsNullOrEmpty(u.LastName)).ToList();
+            listOfLastNameUsers.ForEach(x =>
+            {
+                Console.WriteLine($"{x.FirstName} {x.LastName}");
+            });
+
+            var user = listOfUsers.Where(u => u != null && !string.IsNullOrEmpty(u.LastName)).FirstOrDefault();
+
+            if (user != null)
+            {
+                Console.WriteLine($"{user.FirstName} {user.LastName} and have {user.Email}");
+            }
+
+            var strings = listOfStrings.OrderByDescending(x => x).ToList();
+
+            listOfUsers.Where(x => x != null).GroupBy(x => x.Email).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x.Key);
+
+                x.ToList().ForEach(y =>
+                {
+                    Console.WriteLine(y.FirstName);
+                });
+            });
+        }
+    }
+}
